28c78f9ce4b29659497224d391073f1f060d8b97
Rodrigo de Souza Castro <rodrigo.castro@concrete.com.br>
2017-12-04 10:14:20 -0200

    Add view util extension funcions

--- app/src/main/java/com/castrodev/sharedexpenses/ui/MainActivity.kt
+++ app/src/main/java/com/castrodev/sharedexpenses/ui/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.castrodev.sharedexpenses.R
+import com.castrodev.sharedexpenses.util.bindView
 
 
 class MainActivity : AppCompatActivity() {
 
     private var adapter: CustomFragmentStatePagerAdapter? = null
+
+    private val bottomNavigationView: BottomNavigationView by bindView(R.id.bottom_navigation)
 
     var pager: ViewPager? = null
 
@@ -22,19 +24,12 @@ class MainActivity : AppCompatActivity() {
         pager = findViewById(R.id.pager)
         pager?.adapter = adapter
 
-        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
-
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_favorites -> {
-                    pager?.currentItem = 0
-                }
-                R.id.action_schedules -> {
-                    pager?.currentItem = 1
-                }
-                R.id.action_music -> {
-                    pager?.currentItem = 2
-                }
+            pager?.currentItem = when (item.itemId) {
+                R.id.action_favorites -> 0
+                R.id.action_schedules -> 1
+                R.id.action_music -> 2
+                else -> throw RuntimeException("Unexpected index")
             }
             item.isChecked = true
             false
